b66d42a8e7f9206c6ac6dc82d053ea35a37cf5ac
Martin <martin.runelov@gmail.com>
2013-03-07 12:01:36 +0100

    Added some junk

--- tidaltrust.py
+++ tidaltrust.py
@@ -5,3 +5,23 @@ TidalTrust!
 
 import networkx as nx
 
+
+class TidalTrust:
+
+    # queue.append(x) Adds x to the end of the list.
+    # queue.pop() Removes and returns the last item of the list. Optional argument: index.
+    # len(queue) Returns the length of the list.
+    queue = []
+    
+    # TODO?
+    cached_ratings = {}
+    
+    def tidal_trust(source, sink, graph):
+        """ Calculates a trust value between the source and the sink nodes in the given graph """
+        pass
+    
+    
+    
+
+     
+
