2ef1ee7e081dfc4c0f9fa5d6a7160eb290c4c563
Jonathan <jonte_murray@hotmail.com>
2013-03-07 15:31:31 +0100

    spider that crawls one article and extracts item

---
+++ webcrawler/scrapy/scrapy/spiders/articleSpider.py
@@ -0,0 +1,34 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from xpaths import Xpaths
+
+class ArticleSpider(BaseSpider):
+    name = "article"
+    allowed_domains = ["dn.se"]
+    start_urls = [
+        "http://www.dn.se/nyheter/sverige/abdullah-och-aqilah-fran-irak-lever-pa-en-tusenlapp-var"
+    ]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        xpaths = Xpaths('webpages.xml')
+        article = ArticleItem()
+        
+        for xpath in xpaths.get_title_xpaths("dn.se"):
+            article.title = hxs.select(xpath).extract()
+            break
+        
+        for xpath in xpaths.get_author_xpaths("dn.se"):
+            article.author = hxs.select(xpath).extract()
+            break
+            
+        for xpath in xpaths.get_date_xpaths("dn.se"):
+            article.date = hxs.select(xpath).extract()
+            break
+            
+        for xpath in xpaths.get_summary_xpaths("dn.se"):
+            article.summary = hxs.select(xpath).extract()
+            break
+            
+        return article
+        
