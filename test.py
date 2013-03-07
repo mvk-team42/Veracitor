4b6232909748382e55d2f094e81017c2a1d175dc
Daniel Molin <dmol@kth.se>
2013-03-07 14:59:40 +0100

    fixar vidare :)))

--- test.py
+++ test.py
@@ -3,11 +3,28 @@ import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
 
-G.add_edge(1,2)
-G.add_edges_from([(2,1),(1,3),(3,4),(5,4),(3,2)])
+G.add_weighted_edges_from([(1,2,10),
+                           (1,3,8),
+                           (1,4,9),
+                           (2,5,9),
+                           (3,5,10),
+                           (3,6,10),
+                           (4,5,8),
+                           (4,6,9),
+                           (5,7,8),
+                           (6,7,6),
+                           ])
 
 print G.nodes()
 print G.edges()
+print G[1][2]['weight']
+print nx.to_dict_of_dicts(G)
 
-nx.draw(G)
+#nx.draw(G)
+#nx.draw_circular(G)
+#nx.draw_spectral(G)
+
+shortest = nx.bfs_tree(G, source=1)
+print shortest
+print dir(shortest)
 plt.show()
