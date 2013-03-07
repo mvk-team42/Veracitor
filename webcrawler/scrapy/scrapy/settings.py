df2f607d1823b0e9223af6112063cd840ad0df28
glindste <gustaflindstedt@gmail.com>
2013-03-07 15:04:44 +0100

    scrapy init

---
+++ webcrawler/scrapy/scrapy/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'scrapy'
+
+SPIDER_MODULES = ['scrapy.spiders']
+NEWSPIDER_MODULE = 'scrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy (+http://www.yourdomain.com)'
