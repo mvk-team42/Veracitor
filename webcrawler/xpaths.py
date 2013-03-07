5003f246b66a220bd3bccf7e218f9bc7aae6648d
glindste <gustaflindstedt@gmail.com>
2013-03-07 11:48:50 +0100

    initial xpaths.py

--- webcrawler/xpaths.py
+++ webcrawler/xpaths.py
@@ -1,6 +1,18 @@
 #!/bin/python2.7
 
 import xml.etree.ElementTree as ET
-tree = ET.parse('webpages.xml')
-root = tree.getroot()
+
+class Xpaths:
+
+    def __init__(self, filepath):
+        tree = ET.parse(filepath)
+        self.root = tree.getroot()
+
+    def get_title(self, url):
+
+    def get_author(self, url):
+
+    def get_date(self, url):
+
+    def get_text(self, url):
 
