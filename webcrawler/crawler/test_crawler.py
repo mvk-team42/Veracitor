9c99a8e592f87a3f82e13f58789a0b8aad722da0
glindste <gustaflindstedt@gmail.com>
2013-03-08 14:43:26 +0100

    comments

--- webcrawler/crawler/test_crawler.py
+++ webcrawler/crawler/test_crawler.py
@@ -2,25 +2,36 @@
 from twisted.internet import reactor
 from scrapy.crawler import Crawler
 from scrapy.settings import Settings
-from scrapy import signals
 from scrapy import log
+from scrapy import signals
 from scrapy.xlib.pydispatch import dispatcher
 from crawler.spiders.articleSpider import ArticleSpider
 
 def setup_crawler(domain):
+
+    # connect signals from spiders to methods
+    dispatcher.connect(_item_passed, signal=signals.item_scraped)
+    dispatcher.connect(_spider_opened, signal=signals.spider_opened)
+
+    # init spider and send away!
     spider = ArticleSpider(start_urls=domain)
     crawler = Crawler(Settings())
     crawler.configure()
     crawler.crawl(spider)
-    dispatcher.connect(_item_passed, signal=signals.item_scraped)
     crawler.start()
 
 def _item_passed(item, response, spider):
     print "here!"
     print item
+
+def _spider_opened(spider):
+    print "spider opened!"
     
 
+# insert test articles here
 for domain in ['http://www.dn.se/nyheter/varlden/nordkorea-upphaver-vapenvila-med-syd', 'http://www.nytimes.com/2013/03/08/world/europe/pope-wanted-must-possess-magnetic-charm-and-grit.html']:
     setup_crawler(domain)
+
+# stuff
 log.start()
 reactor.run()
