b3b924f09c520d268b3660e2f45f18e5a84b9410
glindste <gustaflindstedt@gmail.com>
2013-03-08 15:59:04 +0100

    added pipeline

--- webcrawler/crawler/crawler/settings.py
+++ webcrawler/crawler/crawler/settings.py
@@ -11,5 +11,7 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
+ITEM_PIPELINES = ['crawler.pipelines.CrawlerPipeline']
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'crawler (+http://www.yourdomain.com)'
