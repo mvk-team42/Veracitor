af9b34fd721f25714beb62857c7494fac57ce4e8
Jonathan <jonte_murray@hotmail.com>
2013-03-08 14:06:54 +0100

    skeleton for clean text

--- webcrawler/crawler/crawler/items.py
+++ webcrawler/crawler/crawler/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 from scrapy.contrib.loader import ItemLoader, XPathItemLoader
-from scrapy.contrib.loader.processor import TakeFirst
+from scrapy.contrib.loader.processor import TakeFirst, Compose
 
 class ArticleItem(Item):
     title = Field(default="Unknown")
@@ -15,4 +15,7 @@ class ArticleItem(Item):
 
 
 class ArticleLoader(XPathItemLoader):
-    default_output_processor = TakeFirst()
+    default_output_processor = Compose(lambda v: v[0], clean)
+    
+def clean(text):
+    return str.upper(text)
