f32afa97d0961941ad255d9aa2c126f9dda24247
johntu <john.brynte@gmail.com>
2013-03-12 14:43:49 +0100

    Added a basic Flask application.

---
+++ veracitor/app.py
@@ -0,0 +1,20 @@
+import Flask
+
+
+from flask import Flask, render_template
+
+# configuration
+
+app = Flask(__name__)
+try:
+    app.config.from_envvar('VERACITOR_SETTINGS')
+except:
+    app.config.from_pyfile('settings.py')
+
+
+@route("/")
+def index():
+    return render_template("/")
+
+if __name__ == "__main__":
+    app.run()
