f13492cd2a543e144639560287b7e34a0e4f39ac
Daniel Molin <dmol@kth.se>
2013-03-16 17:06:11 +0100

    indent

--- algorithms/test.py
+++ algorithms/test.py
@@ -35,16 +35,16 @@ Gtags = nx.DiGraph()
 # and the "crime" ratings are kind of random.
 # this is how you add edges to the global graph I think y'all
 Gtags.add_edges_from([(1,2,dict(cooking=10, crime=4, weight=1)),
-                               (1,3,dict(cooking=8, crime=7, weight=1)),
-                               (1,4,dict(cooking=9, crime=6, weight=1)),
-                               (2,5,dict(cooking=9, crime=9, weight=1)),
-                               (3,5,dict(cooking=10, crime=5, weight=1)),
-                               (3,6,dict(cooking=10, crime=6, weight=1)),
-                               (4,5,dict(cooking=8, crime=7, weight=1)),
-                               (4,6,dict(cooking=9, crime=6, weight=1)),
-                               (5,7,dict(cooking=8, crime=5, weight=1)),
-                               (6,7,dict(cooking=6, crime=7, weight=1)),
-                               ])
+                      (1,3,dict(cooking=8, crime=7, weight=1)),
+                      (1,4,dict(cooking=9, crime=6, weight=1)),
+                      (2,5,dict(cooking=9, crime=9, weight=1)),
+                      (3,5,dict(cooking=10, crime=5, weight=1)),
+                      (3,6,dict(cooking=10, crime=6, weight=1)),
+                      (4,5,dict(cooking=8, crime=7, weight=1)),
+                      (4,6,dict(cooking=9, crime=6, weight=1)),
+                      (5,7,dict(cooking=8, crime=5, weight=1)),
+                      (6,7,dict(cooking=6, crime=7, weight=1)),
+                      ])
 
 
 #print Gtags[1]
