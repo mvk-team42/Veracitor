6a6672f48b79a820130f96a7dd10e395d96a6c2f
Martin <martin.runelov@gmail.com>
2013-03-20 16:23:06 +0100

    Added more suggestions

--- algorithms/sample_bounds.py
+++ algorithms/sample_bounds.py
@@ -7,19 +7,34 @@ trust evaluation done by SUNNY.
 
 import random
 from numpy import array
+import itertools
 
 
 
-def sample_bounds():
-    pass
+def sample_bounds(bayesianNetwork, k=100):
+    """
+    # TODO Skriva om parameter-texterna :p
+    bayesianNetwork: The nodes to calculate the sample bounds for
+    k : Number of times the sampling loops. A higher value (theoretically) decreases the randomness of the sampling 
+    
+    """
+    # Fast way to iterate k times when an index variable isn't needed according to http://stackoverflow.com/a/2970789/645270 (see comment too)
+    # See simpler alternative below.
+    for _ in itertools.repeat(None, k):
+        print k
+        
+        
+    # Simpler alternative to the above loop 
+    #for _ in range(k):
+        #print k
     
     
     
     
 def _getRandom():
-    # TODO: Välja någon random-funktion.        
-    # random() är snyggare (kortare), men ger [0,1), dvs inkluderar inte 1, vilket vi tekniskt sett ska göra.
-    # Däremot är chansen typ 1 på 100 miljarder att den faktiskt blir 1 i uniform(0,1) :p
+    # TODO: Choose a random function       
+    # random() is nicer (shorter), but returns [0,1), meaning it includes 1, which we technically shouldn't.
+    # But it's about 1 in a billion that uniform(0,1) actually returns 1.
 
     #return random.random
     #return random.uniform(0,1)
