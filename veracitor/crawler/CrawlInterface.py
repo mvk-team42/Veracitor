87e7fc51290d4451268973f495fd759e4793769b
glindste <gustaflindstedt@gmail.com>
2013-03-22 16:59:33 +0100

    dummy crawlinterface

---
+++ veracitor/crawler/CrawlInterface.py
@@ -0,0 +1,14 @@
+def createNewspaperBank():
+    pass
+
+def addNewspaper(url):
+    pass
+
+def requestScrape(newspaper):
+    pass
+
+def startContinuousScrape():
+    pass
+
+def stopContinuousScrape():
+    pass
