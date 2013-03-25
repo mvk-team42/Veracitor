5940a265ad8e1ffacead2f17af38a7a2c57de552
Jonathan <jonte_murray@hotmail.com>
2013-03-25 11:42:35 +0100

    Namechanges and some small additions to code

--- veracitor/crawler/CrawlInterface.py
+++ veracitor/crawler/CrawlInterface.py
@@ -1,14 +1,57 @@
+from crawler.spiders.newspaperBankSpider import NewspaperBankSpider
+from crawler.spiders.metaNewspaperSpider import MetaNewspaperSpider
+from crawler.spiders.rssSpider import RssSpider
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
+from scrapy import log
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
+from crawler.spiders.articleSpider import ArticleSpider
+from crawler.spiders.newspaperSpider import NewspaperSpider
+from time import time
+from scrapy.exceptions import CloseSpider
+import xml.etree.ElementTree as ET
+
+from scrapy.signalmanager import SignalManager
+
+
 def createNewspaperBank():
-    pass
+    _run_spider(NewspaperBankSpider())
 
 def addNewspaper(url):
-    pass
+    _run_spider(MetaNewspaperSpider(url=url))
 
 def requestScrape(newspaper):
-    pass
+    _run_spider(NewspaperSpider(domain=newspaper))
 
 def startContinuousScrape():
-    pass
+    newspaperUrls = []
+    xml_file = "crawler/webpages.xml"
+    tree = ET.parse(xml_file)
+    webpages = tree.getroot().findall("./webpage")
+    for webpage in webpages:
+        newspaperUrls.append(webpage.get("url"))
+    while (True):
+        for url in newspaperUrls:
+            print url
+            spider = RssSpider(url="http://www.dn.se/m/rss/senaste-nytt")
+            _run_spider(spider)
+            
 
 def stopContinuousScrape():
     pass
+    
+    
+def _run_spider(spider):
+    settings = get_project_settings()
+    crawler = Crawler(settings)
+    crawler.configure()
+    crawler.crawl(spider)
+    crawler.start()
+    reactor.run()
+    
+    
+if __name__ == "__main__":
+    startContinuousScrape() 
