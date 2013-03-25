9576a06120be263a4f2247f3736c1c756db8ffd7
glindste <gustaflindstedt@gmail.com>
2013-03-25 19:51:33 +0100

    gjorde om testfil för att testa interface

--- veracitor/crawler/test_crawler.py
+++ veracitor/crawler/test_crawler.py
@@ -1,50 +1,7 @@
 #!/usr/bin/env python2.7
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy.settings import Settings
-from scrapy import log
-from scrapy import signals
-from scrapy.xlib.pydispatch import dispatcher
-from crawler.spiders.articleSpider import ArticleSpider
+import CrawlInterface as ci
 
-# import settings
-from scrapy.utils.project import get_project_settings
+#run tests
+#ci.addNewspaper("www.svd.se")
 
-import crawler.settings as default_settings
-
-def setup_crawler(article_url):
-
-    # connect signals from spiders to methods
-    dispatcher.connect(_item_passed, signal=signals.item_scraped)
-    dispatcher.connect(_spider_opened, signal=signals.spider_opened)
-
-    # init spider
-    spider = ArticleSpider(start_urls=article_url)
-
-    # get project settings and use to init crawler
-    settings = get_project_settings()
-    crawler = Crawler(settings)
-    # print "Pipelines enabled: " + "".join(crawler.settings.get('ITEM_PIPELINES'))
-
-    crawler.configure()
-    crawler.crawl(spider)
-    crawler.start()
-
-def _item_passed(item, response, spider):
-    print "here!"
-    print item
-
-def _spider_opened(spider):
-    print "spider opened!"
- 
- 
-   
-
-# insert test articles here
-urls = ['http://www.dn.se/nyheter/varlden/nordkorea-upphaver-vapenvila-med-syd', 'http://www.nytimes.com/2013/03/08/world/europe/pope-wanted-must-possess-magnetic-charm-and-grit.html']
-for url in urls:
-    setup_crawler(url)
-
-# stuff
-log.start()
-reactor.run()
+ci.requestScrape("www.dn.se")
