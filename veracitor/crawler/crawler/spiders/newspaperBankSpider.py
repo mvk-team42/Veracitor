d9de34b33d881f1c24f414db3bfc1330588f1aae
Jonathan <jonte_murray@hotmail.com>
2013-03-25 09:55:15 +0100

    Changed names of xml-files. Added some spiders

---
+++ veracitor/crawler/crawler/spiders/newspaperBankSpider.py
@@ -0,0 +1,24 @@
+from scrapy.spider import BaseSpider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from crawler.xpaths import Xpaths
+from crawler.items import ArticleItem, ArticleLoader
+from crawler.spiders.articleSpider import ArticleSpider
+from scrapy.contrib.loader import ItemLoader, XPathItemLoader
+from scrapy.contrib.loader.processor import TakeFirst
+from urlparse import urlparse
+
+
+class NewspaperBankSpider(BaseSpider):
+    name = "newspaperBank"
+
+    def __init__(self, *args, **kwargs):
+        super(NewsPaperBankSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ["http://www.listofnewspapers.com/en/europe/newspapers-in-west-midlands.html"]
+
+        
+        
+        
+        
+        
