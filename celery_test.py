44c7be15a87250d787538d3975a786340126d654
Anton Erholt <anton.erholt@gmail.com>
2013-04-02 16:57:08 +0200

    fixed tasks

---
+++ celery_test.py
@@ -0,0 +1,10 @@
+import veracitor.tasks.crawler as tasks
+import unittest
+
+
+class ScrapeTest(unittest.Testcase):
+
+    def test_scrape_article(self):
+        pass
+#        self.assert
+
--- veracitor/tasks/__init__.py
+++ veracitor/tasks/__init__.py
@@ -1,8 +1,7 @@
 # tasks - a package for celery tasks
 # authors : Anton Erholt - <antonaut@github>
 
+from celery import Celery
+
 celery = Celery('veracitor.tasks','mogodb://localhost')
 
-@task
-def add(a, b):
-    return a + b
