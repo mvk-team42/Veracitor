77ea18b56ab803fe272931ed6257915cd6f6b020
Anton Erholt <anton.erholt@gmail.com>
2013-04-02 16:39:27 +0200

    minor fix in tasks

--- veracitor/tasks/__init__.py
+++ veracitor/tasks/__init__.py
@@ -1,10 +1,13 @@
 # tasks - a package for celery tasks
 # authors : Anton Erholt - <antonaut@github>
 
-from celery import Celery, task
+from celery import Celery
 
 celery = Celery('veracitor.tasks','mogodb://localhost')
 
-@task
-def add(a, b):
-    return a + b
+
+if __name__=="__main__":
+
+    @task
+    def add(a, b):
+        return a + b
