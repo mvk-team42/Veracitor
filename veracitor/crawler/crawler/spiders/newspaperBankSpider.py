c0fe12ac0c93c36d0866c4a1affdd758a4101b4b
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2013-04-02 16:41:41 +0200

    bankspider

--- veracitor/crawler/crawler/spiders/newspaperBankSpider.py
+++ veracitor/crawler/crawler/spiders/newspaperBankSpider.py
@@ -4,7 +4,9 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.loader import ItemLoader, XPathItemLoader
 from scrapy.contrib.loader.processor import TakeFirst
+from scrapy import log
 from urlparse import urlparse
+
 from ..xpaths import Xpaths
 from ..items import ArticleItem, ArticleLoader, ProducerItem
 from .articleSpider import ArticleSpider
@@ -16,19 +18,21 @@ class NewspaperBankSpider(CrawlSpider):
 
     def __init__(self, *args, **kwargs):
         
-        self.start_urls = ["http://www.listofnewspapers.com/en/europe/newspapers-in-west-midlands.html"]
         domain = "http://www.listofnewspapers.com"
+
+        self.start_urls = ["http://www.listofnewspapers.com/en/europe/newspapers-in-west-midlands.html"]
         self.rules = (
             Rule(
-                SgmlLinkExtractor(restrict_xpaths = "//li[@class='linewspaper']", deny_domain=domain),
+                SgmlLinkExtractor(restrict_xpaths = "//li[@class='linewspapers']", deny_domains=domain),
                 callback = "parse_webpage_link"
             ),
             Rule(
-                SgmlLinkExtractor(restrict_xpaths = "//li[@class='linewspaper']"), 
+                SgmlLinkExtractor(restrict_xpaths = "//li[@class='linewspapers']", allow_domains=domain)
             ),
         )
+        log.msg("initiated")
         super(NewspaperBankSpider, self).__init__(*args, **kwargs)
         
     def parse_webpage_link(self, response):
+        log.msg("found link")
         return ProducerItem(url = response.url)
-        
