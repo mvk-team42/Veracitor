7596d61389c6915690323898ea132ec8993a3cdf
Daniel Molin <dmol@kth.se>
2013-04-15 15:42:47 +0200

    Converts string to unicode in tidaltrust task

--- veracitor/tasks/algorithms.py
+++ veracitor/tasks/algorithms.py
@@ -16,8 +16,16 @@ def sunny(source, sink, tag, network):
 
 @taskmgr.task
 def tidaltrust(source, sink, tag):
+    # Convert input to unicode
+    source = unicode(source)
+    sink = unicode(sink)
+    tag = unicode(tag)
+
+    # Get global network
     network = gn.get_global_network()
     trust = compute_trust(network=network,
                           source=source,
                           sink=sink, tag=tag)
+
+    print trust
     return trust
