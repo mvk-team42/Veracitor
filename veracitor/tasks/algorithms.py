66652c791a99e969ab55c4f465397c5935a0807e
Daniel Molin <dmol@kth.se>
2013-04-15 15:07:25 +0200

    Finished connection jobs->tasks for tidaltrust

--- veracitor/tasks/algorithms.py
+++ veracitor/tasks/algorithms.py
@@ -8,14 +8,16 @@ except:
     from tasks import taskmgr
 
 from ..algorithms.tidaltrust import compute_trust
+from ..database import globalNetwork as gn
 
 @taskmgr.task
 def sunny(source, sink, tag, network):
     pass
 
 @taskmgr.task
-def tidaltrust(source, sink, tag, network, decision=None):
+def tidaltrust(source, sink, tag):
+    network = gn.get_global_network()
     trust = compute_trust(network=network,
                           source=source,
-                          sink=sink, tag=tag,decision=decision)
+                          sink=sink, tag=tag)
     return trust
