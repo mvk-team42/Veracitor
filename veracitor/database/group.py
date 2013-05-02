1292be9bd7532a1ce645fe4f317914000f8aba8b
krappman <krappman@kth.se>
2013-05-02 17:07:03 +0200

    Added trigger to group save() so that the owner is updated.

--- veracitor/database/group.py
+++ veracitor/database/group.py
@@ -8,7 +8,10 @@
 """
 
 from mongoengine import *
-
+import networkModel
+import datetime
+import user
+import extractor
 connect('mydb')
 
 class Group(Document):
@@ -21,11 +24,24 @@ class Group(Document):
     """
     name = StringField(required=True) 
     description = StringField()
-    owner = ReferenceField(('User'), required=True)
+    owner = ReferenceField(user.User, required=True)
     #tags = ListField(ReferenceField('Tag'), required=True)
     producers = ListField(ReferenceField('Producer'))
     time_created = DateTimeField(required=True)
     
-    
-    
-    
+    def save(self):
+        first_time = False
+        if(not extractor.contains_group(self.name)):
+            first_time = True
+        super(Group, self).save()
+        if(first_time):
+            self.owner.groups.append(self)
+            self.owner.save()
+
+if __name__ == "__main__":
+    networkModel.build_network_from_db()
+    u1 = user.User(name="hurseeee", password="123")
+    u1.save()
+    g1 = Group(name="kdjdkj", owner=u1, time_created=datetime.datetime.now())
+    g1.save()
+    print u1.groups[0].name
