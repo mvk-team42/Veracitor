16b8342af01d1468a9f5561d1833043dc470a55a
mrunelov <mrunelov@kth.se>
2013-05-08 13:58:16 +0200

    Fixed fetching of information and some template issues

--- veracitor/web/ratings.py
+++ veracitor/web/ratings.py
@@ -243,7 +243,9 @@ def get_used_info_tags():
     try:
         # TODO: Try this! (nested list comprehension)
         user = extractor.get_user(session['user_name'])
-        tags_used = list(set((tag.name for tag in ir.information.tags) for ir in user.info_ratings))
+        tags_used = []
+        for info_rating in user.info_ratings:
+            tags_used.append(extractor.get_information(__safe_string(info_rating)).tags)
 
         return jsonify(tags=tags_used)
     except:
@@ -256,3 +258,7 @@ def get_all_tags():
         return tag_names
     except:
         abort(400)
+
+
+def __safe_string(url):
+    return url.replace("|", ".")
