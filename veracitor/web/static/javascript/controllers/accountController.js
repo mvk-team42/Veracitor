82693a21a76ba790d455aca0b548f9eea2f49987
johntu <johntu@kth.se>
2013-05-10 15:36:47 +0200

    Added focus and bound key in account login tab.

--- veracitor/web/static/javascript/controllers/accountController.js
+++ veracitor/web/static/javascript/controllers/accountController.js
@@ -8,32 +8,46 @@
  */
 var AccountController = function (controller) {
 
+    var ENTER = 13;
+
     /**
        This function is called by the super controller when the tab is opened.
      */
     this.on_tab_active = function () {
+        // Set focus to the user name text field
+        $('#account_view > .content > .register-content > .register-form > input[name=username]').focus();
+    };
 
-      $("#register-button").click(function(){
-        var username = $(".register-form input[name='username']").val(),
-        password = $(".register-form input[name='password']").val();
-        console.log("username: " + username +" pass: " + password);
-        $.post("/register",{"username": username,
-                            "password": password},
-              function(data){
+    (function () {
+        $("#register-button").click(function(){
+            var username = $(".register-form input[name='username']").val(),
+            password = $(".register-form input[name='password']").val();
+            console.log("username: " + username +" pass: " + password);
+            $.post("/register", {
+                "username": username,
+                "password": password
+            }, function(data){
                 var job_id = data['job_id'];
                 controller.set_job_callback(job_id, function(d){
-                  console.log(d);
-                  var res = d.result;
-                  if (res['user_created']) {
-                    $(".register-content").html("User created! Please use login tab to login.");
-                  } else if (typeof(res['error']) !== 'undefined' && res['error'] !== null) {
-                    $(".register-content .error").html(res['error']);
-                  }
+                    var res = d.result;
+
+                    if (res['user_created']) {
+                        $(".register-content").html("User created! Please use login tab to login.");
+                    } else if (typeof(res['error']) !== 'undefined' && res['error'] !== null) {
+                        $(".register-content .error").html(res['error']);
+                    }
                 });
-                
+
                 $('.register-content .error').html("Registering...");
-              });
-      });
-    };
+            });
+        });
+
+        // Bind the enter key to the submit button
+        $('#account_view > .content > .register-content > .register-form > .field').keydown(function (evt) {
+            if (evt.keyCode === ENTER) {
+                $("#register-button").click();
+            }
+        });
+    })();
 
 };
