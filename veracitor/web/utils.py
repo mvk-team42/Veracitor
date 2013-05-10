4e60e0a70069b07c0c3426b144f90d0c98c34fb3
mrunelov <mrunelov@kth.se>
2013-05-10 15:40:00 +0200

    Fixed workarounds for db. Now working

--- veracitor/web/utils.py
+++ veracitor/web/utils.py
@@ -41,24 +41,23 @@ def get_user_as_dict(username):
 
     try:
         source_ratings = []
+        
         for s in user_obj.source_ratings.keys():
             for tag in user_obj.source_ratings[s]:
                 source_ratings.append({
                         'name' : s,
                         'tag' : tag,
                         'rating': user_obj.source_ratings[s][tag] ,
-                        'description': extractor.get_producer(s).description})  
-                        
+                        'description': extractor.get_producer(__safe_string(s)).description})  
+             
         info_ratings = []
-        
         for iurl in user_obj.info_ratings.keys():
-            log(iurl)
             info_ratings.append({
-                    'title': extractor.get_information(iurl).title,
-                    'rating': user_obj.info_ratings[__safe_string(iurl)],
-                    'url': iurl,
+                    'title': extractor.get_information(__safe_string(iurl)).title,
+                    'rating': user_obj.info_ratings[iurl],
+                    'url': __safe_string(iurl),
                     })
-            
+        
         groups = [{'name' : g.name,
                    'description' : g.description,
                    'producers' : [pname for pname in g.producers.keys()]}
