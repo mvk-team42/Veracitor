5a12d81300a240944f685421413b748bbfad7262
mrunelov <mrunelov@kth.se>
2013-05-14 13:17:36 +0200

    Fixed minor errors in sunny and added get_difference_on_extremes to networkModel

--- veracitor/algorithms/sunny/gentest.py
+++ veracitor/algorithms/sunny/gentest.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import sample_bounds as sb
+import sunny
 
 G = nx.DiGraph()
-G.add_edges_from([(u'1',u'2',dict(cooking=12))])
-print sb.sample_bounds(G,'1','2',100)
+G.add_edges_from([(u'mrunelov',u'alfred',dict(cooking=12))])
+sunny.sunny(G,'mrunelov','alfred')
