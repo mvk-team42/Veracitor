a14fff1cf5cb57bee880ccde521024b864825ea0
mrunelov <mrunelov@kth.se>
2013-05-29 15:09:48 +0200

    Changed rounding to 1 decimal in tidal trust

--- veracitor/algorithms/tidaltrust.py
+++ veracitor/algorithms/tidaltrust.py
@@ -128,7 +128,7 @@ def tidal_trust(source, sink, graph, tag):
         
     
     if (source, sink) in cached_trust:
-        results["trust"] = round(cached_trust[(source, sink)],2)
+        results["trust"] = round(cached_trust[(source, sink)],1)
 
     return results        
     
